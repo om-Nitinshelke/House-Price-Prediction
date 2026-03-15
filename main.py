328153ae1b2cc8d8a6d9b5664f6dcf419e9631e5
Om <shelkeom67@gmail.com>
2026-03-15 11:24:54 +0530

    restore accidently deleted code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.data_loader import load_data
 from src.split import split_data
+import pandas as pd
 
 
 df=load_data()
@@ -8,5 +9,8 @@ train_set,test_set=split_data(df,0.2)
 
 print("Train set:",len(train_set))
 print("Test set:",len(test_set))
+
+
+
 
 
